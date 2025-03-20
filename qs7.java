a0183dd0e2591f09c4b9b2bae68992fff9959dd4
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-03-20 23:12:32 +0530

    Fourth Commit

--- qs7.java
+++ qs7.java
@@ -1,56 +1,35 @@
 import java.util.Scanner;
 
 public class qs7 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        
-        System.out.println("Enter size of the row");
         int m = sc.nextInt();
-        System.out.println("Enter size of the column");
         int n = sc.nextInt();
-        
-        
         int[][] arr = new int[m][n];
-        System.out.println("Enter elements of the array");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        
-       
+        int svj = 0;
         for (int i = 0; i < m; i++) {
-            int minRow = arr[i][0];
-            int minCol = 0;
-            
-            
             for (int j = 1; j < n; j++) {
-                if (arr[i][j] < minRow) {
-                    minRow = arr[i][j];
-                    minCol = j;
+                if (arr[i][j] < arr[i][svj]) {
+                    svj = j;
                 }
             }
-
-            
-            boolean isSaddlePoint = true;
+            boolean found = true;
             for (int k = 0; k < m; k++) {
-                if (arr[k][minCol] > minRow) {
-                    isSaddlePoint = false;
-                    break;
+                if (arr[k][svj] > arr[i][svj]) {
+                    found = false;
                 }
             }
-
-            
-            if (isSaddlePoint) {
-                System.out.println("SaddlePoint "+arr[i][minCol]);
-              
-                break; 
+            if (found) {
+                System.out.println(arr[i][svj]);
+                break;
             }
         }
-
-        
 
         sc.close();
     }
